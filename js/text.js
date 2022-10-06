2265599538485232d044c1fb7313ce6b0afea3c0
Higor Alves <higoralvesmtv@gmail.com>
2022-10-06 17:08:06 -0300

    modified files js and index

--- js/text.js
+++ js/text.js
@@ -1,6 +1,6 @@
 
 const el = document.querySelector("#text");
-const text = "Sou Higor Alves Fonseca! Adoro programar, além de tocar violão, tirar fotos e aprender coisas novas! Sinta-se à vontade para entrar em contato ou dar uma olhada nos meus trabalhos anteriores abaixo.";
+const text = "Olá, sou o Higor!";
 const interval = 150;
 
 function showText(el, text, interval){
